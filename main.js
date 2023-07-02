0cbe9beab378b5150bdba411f2023aabcdd2068d
jiniviolet <haejin0154@naver.com>
2023-07-02 15:13:35 +0900

    step3 template

---
+++ main.js
@@ -0,0 +1,28 @@
+const articles = document.querySelectorAll('article');
+const aside = document.querySelector('aside');
+const btnClose = aside.querySelector('.btnClose');
+
+articles.forEach((article, index)=>{
+    article.addEventListener('mouseenter', e => {
+        e.currentTarget.querySelector('video').play();
+    })
+
+        article.addEventListener('mouseleave', e => {
+            e.currentTarget.querySelector('video').pause();
+        })
+
+        article.addEventListener('click', e => {
+            const tit = e.currentTarget.querySelector('h2').innerText;
+            const con = e.currentTarget.querySelector('p').innerText;
+            const vidsrc = e.currentTarget.querySelector('video').getAttribute('src');
+
+            aside.querySelector('h2').innerText = tit;
+            aside.querySelector('p').innerText = con;
+            aside.querySelector('video').setAttribute('src', vidsrc);
+            aside.classList.add('on');
+        })
+
+        btnClose.addEventListener('click', ()=>{
+            aside.classList.remove('on');
+        })
+})
